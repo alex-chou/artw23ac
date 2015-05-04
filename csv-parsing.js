081220e68c0cd130011a8d67b37b903e7da5140c
Alex Chou <chou.alexander@berkeley.edu>
2015-05-03 22:04:15 -0700

    fix syntax

--- csv-parsing.js
+++ csv-parsing.js
@@ -2,7 +2,7 @@ d3.csv("pre_assessment.csv", function(data) {
   var keys = Object.keys(data[0]);
   keys.forEach(function(k) {
     console.log(k);
-  }
+  });
   data.forEach(function(d) {
     console.log(d);
   });

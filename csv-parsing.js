615c0b77649cd81e2964988643702de5ceac8add
Alex Chou <chou.alexander@berkeley.edu>
2015-05-03 20:43:07 -0700

    wrong variable

--- csv-parsing.js
+++ csv-parsing.js
@@ -1,5 +1,5 @@
 d3.csv("pre_assessment.csv", function(data) {
-    var parsedData = d3.csv.parseRows(unparsedData);
+    var parsedData = d3.csv.parseRows(data);
     for (i = 0; i < parsedData.length; i++) {
       console.log("hi");
       console.log(parsedData[i]);

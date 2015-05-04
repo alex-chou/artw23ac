5f6e0ce8b34ab28824975ffe80586e61e3da6490
Alex Chou <chou.alexander@berkeley.edu>
2015-05-04 10:50:33 -0700

    add in csv parse function

--- csv-parsing.js
+++ csv-parsing.js
@@ -1,9 +1,12 @@
-d3.csv("pre_assessment.csv", function(data) {
-  var keys = Object.keys(data[0]);
-  keys.forEach(function(k) {
-    console.log(k);
+parseCSV = function(csvPath) {
+  d3.csv(csvPath, function(data) {
+    var keys = Object.keys(data[0]);
+    keys.forEach(function(k) {
+      console.log(k);
+    });
+    data.forEach(function(d) {
+      console.log(d);
+    });
   });
-  data.forEach(function(d) {
-    console.log(d);
-  });
-});
+};
+

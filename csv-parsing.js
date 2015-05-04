cb8d510b9b8adb7732bbbd7b5c9d799be779d3e1
Alex Chou <chou.alexander@berkeley.edu>
2015-05-04 11:58:46 -0700

    dynamically add checkboxes

--- csv-parsing.js
+++ csv-parsing.js
@@ -2,8 +2,14 @@ function parseCSV(csvPath) {
   if (csvPath.match(/\.csv$/)) {
     d3.csv(csvPath, function(data) {
       var keys = Object.keys(data[0]);
+      var container = $('parameters');
+      var inputs = container.find('input');
+      inputs.forEach(function(input) {
+        container.removeChild(input);
+      });
       keys.forEach(function(k) {
-        console.log(k);
+        $('<input />', { type: 'checkbox', id: 'cb' + id, value: k }).appendTo(container);
+        $('<label />', { 'for': 'cb' + id, text: k }).appendTo(container);
       });
       data.forEach(function(d) {
         console.log(d);

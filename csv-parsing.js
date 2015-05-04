b98b6f3b84b4c1827448e26e3c821b56343bcdd3
Alex Chou <chou.alexander@berkeley.edu>
2015-05-03 22:00:26 -0700

    update foreach loop

--- csv-parsing.js
+++ csv-parsing.js
@@ -1,7 +1,7 @@
 d3.csv("pre_assessment.csv", function(data) {
-  for (i = 0; i < data.length; i++) {
-    console.log(data[i]);
-  }
+  data.forEach(function(d) {
+    console.log(d);
+  });
   var parsedData = d3.csv.parseRows(data);
   console.log(parsedData);
 });

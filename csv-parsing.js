f040ecd234a372b41bec6ba4bf12d906309a6a00
Alex Chou <chou.alexander@berkeley.edu>
2015-05-03 20:18:35 -0700

    parsing data

--- csv-parsing.js
+++ csv-parsing.js
@@ -1,3 +1,4 @@
-d3.text("pre_assessment.csv", function(data) {
-    console.log(data);
+d3.csv("pre_assessment.csv", function(data) {
+    var parsedData = d3.csv.parseRows(unparsedData);
+    console.log(parseData);
 });

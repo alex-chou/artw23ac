ae24add27a29127613a0656e0585162ac33b4e2a
Kartik Jhingan <kkjhingan@gmail.com>
2015-05-07 22:59:37 -0700

    avg attempt 3

--- csv-parsing.js
+++ csv-parsing.js
@@ -88,7 +88,7 @@ function createBar(title, side, data) {
 
   svg.append("text").attr("x", w / 2).attr("y", - margin.top / 2).attr("text-anchor", "middle")
                     .style("text-decoration", "underline").style("font-size", "16px").text(title);
-  svg.append("text").attr("x", w / 4).attr("y", - margin.top / 4).attr("text-anchor", "middle")
+  svg.append("text").attr("x", w / 2).attr("y", - margin.top / 3).attr("text-anchor", "middle")
                     .style("text-decoration", "underline").style("font-size", "16px").text(avg);
   svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + h + ")").call(xAxis);
   svg.append("g").attr("class", "y axis").call(yAxis);
